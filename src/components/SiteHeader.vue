cfccdb1273e11db94369a8c6673c3570ed0c55c8
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-10 14:38:57 +0100

    add header structure

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+    name:'SiteHeader'
+}
+
+</script>
+
+<template>
+
+    <header id="site_header" class="debug">
+        <div class="container">
+            <div class="logo"></div>
+            <nav class="navbar">
+                <ul>
+                    <li>Lorem.</li>
+                    <li>Lorem.</li>
+                    <li>Lorem.</li>
+                    <li>Lorem.</li>
+                    <li>Lorem.</li>
+                    <li>Lorem.</li>
+                    <li>Lorem.</li>
+                    <li>Lorem.</li>
+                    <li>Lorem.</li>
+                    <li>Lorem.</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+</template>
+
+<style scoped>
+header{
+    height:300px;
+    background-color: red;
+}
+.container{
+    max-width: 1140px;
+    margin:0 auto;
+    height:300px;
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+}
+.debug .logo{
+    width:30%;
+    height:300px;
+    background-color: blue;
+}
+
+.debug .navbar{
+    background-color: green;
+}
+.debug .navbar ul{
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+}
+
+</style>
