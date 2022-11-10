c8b0ae3b4138c9f84b004f02ec1319229b058017
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-10 18:22:38 +0100

    add bonus

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import SiteHeader from './components/SiteHeader.vue';
 import SiteMain from './components/SiteMain.vue';
+import MainFeatures from './components/MainFeatures.vue';
 import SiteFooter from './components/SiteFooter.vue';
 
 
@@ -9,7 +10,8 @@ export default {
     components: { 
         SiteHeader,
         SiteMain,
-        SiteFooter
+        SiteFooter,
+        MainFeatures
     }
 }
 
---
+++ src/components/MainFeatures.vue
@@ -0,0 +1,47 @@
+<script>
+
+export default{
+    name: 'MainFeatures',
+}
+
+</script>
+
+<template>
+<div class="features">
+    <div class="container">
+        <div class="card">
+            <div>
+                <img src="/img/buy-comics-digital-comics.png" alt="">
+            </div>
+            <span>
+                Digital comics
+            </span>
+        </div>
+        <div class="card">
+            <img src="/img/buy-comics-merchandise.png" alt="">
+            <span>
+                dc merchandise
+            </span>
+        </div>
+        <div class="card">
+            <img src="/img/buy-comics-subscriptions.png" alt="">
+            <span>
+                Subscriptions
+            </span>
+        </div>
+        <div class="card">
+            <img src="/img/buy-comics-shop-locator.png" alt="" id="locator">
+            <span>
+                comic shop locator
+            </span>
+        </div>
+        <div class="card">
+            <img src="/img/buy-dc-power-visa.svg" alt="">
+            <span>
+                dc power visa
+            </span>
+        </div>               
+    </div>
+</div>
+</template>
+
