f2b77ec39a4e0595f72b0aa5cf52f84d70aa29f3
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-11 16:13:05 +0100

    add comics.js in separate file

--- src/assets/data/comics.js
+++ src/assets/data/comics.js
@@ -1,8 +1,5 @@
-export default {
-    name:'comics',
-    data(){
-        return{
-           comics: [
+
+const comics = [
                 {
                   "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
                   "price": "$19.99",
@@ -76,6 +73,5 @@ export default {
                   "type": "graphic novel"
                 }
               ]
-        }
-    }
-}
+  
+export default comics
